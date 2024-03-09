847b64c5ef8e666a2db9c532ea25def190db3c67
web3santa <usefulstory35@gmail.com>
2024-03-10 04:46:56 +0900

    set webapp

--- main.go
+++ main.go
@@ -1,67 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
 )
 
-type Person struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	HairColo  string `json:"hair_color"`
-	HasDog    bool   `json:"has_dog"`
-}
-
 func main() {
-	myJson := `
-[
-    {
-        "first_name": "Clark",
-        "last_name": "Kent",
-        "hair_color": "black",
-        "has_dog": true
-    },
-    {
-        "first_name": "Bruce",
-        "last_name": "Wayne",
-        "hair_color": "black",
-        "has_dog": false
-    }
-]`
 
-	var unmarshalled []Person
-	err := json.Unmarshal([]byte(myJson), &unmarshalled)
-	if err != nil {
-		log.Println("Error unmarshalling json")
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello world")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
 
-	log.Println("unmarshalled: %v", unmarshalled)
-
-	// write json from a struct
-	var mySlice []Person
-
-	var m1 Person
-	m1.FirstName = "Wally"
-	m1.LastName = "West"
-	m1.HairColo = "Red"
-	m1.HasDog = false
-
-	mySlice = append(mySlice, m1)
-
-	var m2 Person
-	m2.FirstName = "Alex"
-	m2.LastName = "King"
-	m2.HairColo = "Green"
-	m2.HasDog = true
-
-	mySlice = append(mySlice, m2)
-
-	newJson, err := json.MarshalIndent(mySlice, "", "    ")
-	if err != nil {
-		log.Println("error marshalling", err)
-	}
-
-	fmt.Println(string(newJson))
-
+	_ = http.ListenAndServe(":8080", nil)
 }
