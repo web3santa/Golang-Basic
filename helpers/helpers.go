da1a064b2da7798a0d9a4e5842d53f208997d0f5
web3santa <usefulstory35@gmail.com>
2024-03-10 04:11:43 +0900

    use json

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,12 @@
 package helpers
 
-type SomeType struct {
-	TypeName   string
-	TypeNumber int
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomNumber(n int) int {
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(n)
+	return value
 }
--- main.go
+++ main.go
@@ -1,14 +1,67 @@
 package main
 
-import "main.go/helpers"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColo  string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
+}
 
 func main() {
-	println("Hello")
+	myJson := `
+[
+    {
+        "first_name": "Clark",
+        "last_name": "Kent",
+        "hair_color": "black",
+        "has_dog": true
+    },
+    {
+        "first_name": "Bruce",
+        "last_name": "Wayne",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
 
-	var myVar helpers.SomeType
-	myVar.TypeName = "Some name"
-	myVar.TypeNumber = 42
+	var unmarshalled []Person
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println("Error unmarshalling json")
+	}
 
-	println("TypeName:", myVar.TypeName)
-	println("TypeNumber:", myVar.TypeNumber)
+	log.Println("unmarshalled: %v", unmarshalled)
+
+	// write json from a struct
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColo = "Red"
+	m1.HasDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Alex"
+	m2.LastName = "King"
+	m2.HairColo = "Green"
+	m2.HasDog = true
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "    ")
+	if err != nil {
+		log.Println("error marshalling", err)
+	}
+
+	fmt.Println(string(newJson))
+
 }
