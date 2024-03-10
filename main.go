8d5ce07062f4ab41b6943cbf2b0999170b7c1252
web3santa <usefulstory35@gmail.com>
2024-03-11 01:05:58 +0900

    serving html

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -10,37 +10,21 @@ const portNumber = ":3000"
 
 // home is the ome page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "THis is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is ather about apge and 2+ 2 is %d", sum))
+	renderTemplate(w, "about.page.tmpl")
 }
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 10.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Connot divde by 0")
+		fmt.Println("Error parsing template", err)
 		return
 	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", 100.0, 10.0, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y < 0 {
-		err := errors.New("Connot divde by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
-// advalue add two integres and return the sum
-func addValues(x, y int) int {
-	return x + y
 }
 
 // main is the main application
@@ -48,7 +32,6 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
